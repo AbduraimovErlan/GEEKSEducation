221a57be2de10c6fffd151adc2e137912eb55a09
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-21 13:48:52 -0400

    task_3

--- task.py
+++ task.py
@@ -39,3 +39,16 @@ class Solution:
             else:
                 list_.append(str(i))
         return list_
+
+
+
+
+class Solution:
+    def twoSum(self, nums: list, target: int):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
+
+
