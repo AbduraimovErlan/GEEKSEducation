63f6df23ce9ef1075d6bee5abc82bd727a52e497
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-21 13:46:46 -0400

    task_2

--- task.py
+++ task.py
@@ -23,3 +23,19 @@ class Solution(object):
     : type s: str
     :rtype: int
     """
+
+
+
+class Solution:
+    def fizzBuzz(self, n: int):
+        list_ = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                list_.append("fizzBuzz")
+            elif i % 3 == 0:
+                list_.append("fizz")
+            elif i % 5 == 0:
+                list_.append("Buzz")
+            else:
+                list_.append(str(i))
+        return list_
