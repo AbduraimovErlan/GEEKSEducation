dcb76d36110cca6a5daa87907ba6d48d95e781da
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-24 13:23:41 -0400

    for example5

--- class_and_function.py
+++ class_and_function.py
@@ -178,3 +178,121 @@ class RightTriangle(Figure):
 figure2_list = [Circle(4), Circle(6), RightTriangle(2, 4), RightTriangle(6, 7), RightTriangle(5, 7)]
 for j in figure2_list:
     print(j.ingo())
+
+
+
+
+
+
+
+
+
+class Computer:
+    def __init__(self, cpu, memory):
+        self.__cpu = cpu
+        self.__memory = memory
+
+
+    @property
+    def cpu(self):
+        return self.__cpu
+
+    @cpu.setter
+    def cpu(self, value):
+        self.__cpu = value
+
+
+    @property
+    def memory(self):
+        return self.__memory
+
+    @memory.setter
+    def memory(self, value):
+        self.__memory = value
+
+
+    def make_computations(self):
+        return self.__memory * self.__cpu
+
+
+    def __str__(self):
+        return f'Оператативная память: {self.memory}, Тактовая частота: {self.cpu}'
+
+
+    def __gt__(self, other):
+        return self.memory > other.memory
+
+    def __ge__(self, other):
+        return self.memory > other.memory
+
+    def __lt__(self, other):
+        return self.memory < other.memory
+
+    def __le__(self, other):
+        return self.memory < other.memory
+
+    def __eq__(self, other):
+        return self.memory == other.memory
+
+    def __ne__(self, other):
+        return self.memory != other.memory
+
+
+
+class Phone:
+    def __init__(self, sim_cards_list):
+        self.__sim_cards_list = sim_cards_list
+
+
+    @property
+    def sim_cards_list(self):
+        return self.__sim_cards_list
+
+    @sim_cards_list.setter
+    def sim_cards_list(self, value):
+        self.__sim_cards_list = value
+
+
+
+    def call(self, sim_card_number, call_to_number):
+        print(f"'Идет звонок на номер {call_to_number}' с сим-карты-{sim_card_number} - "
+              f"{self.__sim_cards_list[sim_card_number-1]}")
+
+
+    def __str__(self):
+        return f'Список сим-кард: {self.sim_cards_list}'
+
+class SmartPhone(Computer, Phone):
+    def __init__(self, cpu, memory, sim_cards_list):
+        Computer.__init__(self, cpu, memory)
+        Phone.__init__(self, sim_cards_list)
+
+
+    @staticmethod
+    def use_gps(location):
+        print(f"Маршрут до {location} построен!")
+
+    def __str__(self):
+        return f'Список сим-кард: {self.sim_cards_list}, ' \
+               f'Оперативная память: {self.memory}, Тактовая частота: {self.cpu}'
+
+
+nokia_phone = Phone(['O!', 'Beeline'])
+simple_computer = Computer('5.8 GHz', 32)
+simple2_computer = Computer('6 GHz', 64)
+redmi_smartphone = SmartPhone('4.4 GHz', 32, ['Megafon', 'MTS'])
+samsung_smartphone = SmartPhone('4.5 GHz', 68, ['MTZ', 'TeLe2'])
+redmi_smartphone.call(1, '+7(812)336-42-42')
+nokia_phone.call(2, '+99677701-23-60')
+redmi_smartphone.use_gps('Ош базар')
+
+
+print(simple2_computer > samsung_smartphone)
+print(simple2_computer == simple_computer)
+print(redmi_smartphone != samsung_smartphone)
+print(redmi_smartphone <= samsung_smartphone)
+
+
+items_list = [simple_computer, nokia_phone, redmi_smartphone, samsung_smartphone]
+for items in items_list:
+    print(items)
